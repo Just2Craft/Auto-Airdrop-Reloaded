d74a80cd540fe9187d3f97eba879416e385638c5
SpyL1nk <exo.war@gmail.com>
2015-08-19 00:41:27 +0200

    Transfert vers Maven, identation du code

--- src/me/Vedronex/AAD/Listeners.java
+++ src/me/Vedronex/AAD/Listeners.java
@@ -12,29 +12,48 @@ import org.bukkit.inventory.ItemStack;
 
 import me.Vedronex.AAD.AutoAirDrop;
 
-public class Listeners implements Listener{
+public class Listeners implements Listener {
 
 	public AutoAirDrop plugin;
-	public Listeners(AutoAirDrop plugin){this.plugin = plugin;}
 
-	@EventHandler (priority = EventPriority.LOWEST)
-	public void onBlockBreak(BlockBreakEvent e){
+	public Listeners(AutoAirDrop plugin) {
+		this.plugin = plugin;
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onBlockBreak(BlockBreakEvent e) {
 		Block b = e.getBlock();
-		if(b.getType() == Material.CHEST){
+		if (b.getType() == Material.CHEST) {
 			String x = Integer.toString(b.getX()) + "-" + Integer.toString(b.getY()) + "-" + Integer.toString(b.getZ());
-			if(plugin.LocList.contains(x)){plugin.LocList.remove(x);return;}	
-	}}
-	
-	@EventHandler (priority = EventPriority.LOWEST)
-	public void onInventoryClose(InventoryCloseEvent e){
-		if(e.getInventory().getHolder() instanceof Chest){
-			Chest c = (Chest)e.getInventory().getHolder();
+			if (plugin.LocList.contains(x)) {
+				plugin.LocList.remove(x);
+				return;
+			}
+		}
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onInventoryClose(InventoryCloseEvent e) {
+		if (e.getInventory().getHolder() instanceof Chest) {
+			Chest c = (Chest) e.getInventory().getHolder();
 			Block b = c.getBlock();
-			String loc = Integer.toString(b.getX()) + "-" + Integer.toString(b.getY()) + "-" + Integer.toString(b.getZ());
-			if(plugin.LocList.contains(loc)){
+			String loc = Integer.toString(b.getX()) + "-" + Integer.toString(b.getY()) + "-"
+					+ Integer.toString(b.getZ());
+			if (plugin.LocList.contains(loc)) {
 				ItemStack items[] = c.getInventory().getContents();
 				int x = 0;
-				for (ItemStack item: items){if(item != null){x = 1;}}
-				if(x == 0){plugin.LocList.remove(loc);if(plugin.DEL){b.setType(Material.AIR);}
-	}}}}
+				for (ItemStack item : items) {
+					if (item != null) {
+						x = 1;
+					}
+				}
+				if (x == 0) {
+					plugin.LocList.remove(loc);
+					if (plugin.DEL) {
+						b.setType(Material.AIR);
+					}
+				}
+			}
+		}
+	}
 }
